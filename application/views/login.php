65f8f5ba02bb828b9f189f87ea0c5f1f27b21c78
Yudha Aris Kristanto <yudhaaris898@gmail.com>
2025-09-07 01:08:23 +0700

    menambahkan fitur login

--- application/views/login.php
+++ application/views/login.php
@@ -13,7 +13,6 @@
                 </div>
             </div>
         </header>
-        
         <!-- Main Content-->
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
